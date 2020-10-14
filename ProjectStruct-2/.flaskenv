aafb08c5b0f669183a010682614199f78490868a
Benny Jane <3355817143@qq.com>
2020-10-14 23:02:25 +0800

    添加第二种文件结构

--- ProjectStruct-2/.flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=FlaskAppFile
-FLASK_ENV=development
--- ProjectStruct-2/simpleApp/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time : 2020/9/26
-# @Author : Benny Jane
-# @Email : 暂无
-# @File : __init__.py.py
-# @Project : Flask-Demo
