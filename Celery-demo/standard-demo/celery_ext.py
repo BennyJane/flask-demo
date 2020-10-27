10aaf3221439af714ab21087b2245f5cb8b021d0
Benny Jane <3355817143@qq.com>
2020-10-27 15:04:39 +0800

    测试celery框架,制作标准插入模板

---
+++ Celery-demo/standard-demo/celery_ext.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+
+class CeleryConfig:
+    BROKER_URL = 'redis://localhost:6379'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+    CELERY_IMPORTS = ('app',)
+
+# todo 第一个参数以及 imports 必须是异步任务所在模块的名称
+celery_app = Celery('app')
+celery_app.config_from_object(CeleryConfig)
